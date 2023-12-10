0422ab67a2a8a2f23a20444a192c976916235675
souha <souhaguezguez@gmail.com>
2023-12-10 15:15:40 +0100

    ajout de intro et home page

--- app/src/main/java/essths/li3/e_commerceproject/homeActivity.java
+++ app/src/main/java/essths/li3/e_commerceproject/homeActivity.java
@@ -1,16 +1,14 @@
 package essths.li3.e_commerceproject;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
+public class homeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "This is our project ", Toast.LENGTH_SHORT).show();
+
     }
 }
---
+++ app/src/main/java/essths/li3/e_commerceproject/introActivity.java
@@ -0,0 +1,26 @@
+package essths.li3.e_commerceproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class introActivity extends AppCompatActivity {
+    ConstraintLayout startbtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        startbtn=findViewById(R.id.startbtn);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(introActivity.this,homeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
